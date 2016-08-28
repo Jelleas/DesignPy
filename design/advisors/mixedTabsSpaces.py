9dbc87198cdc0b7fd48a49bb23a8ff1db4fac132
Jelleas <jelle.van.assema@gmail.com>
2016-08-28 14:20:35 +0200

    added mixed tabs and spaces advisor

--- design/advisors/mixedTabsSpaces.py
+++ design/advisors/mixedTabsSpaces.py
@@ -2,4 +2,20 @@ import advice as a
 import lib
 
 def advice():
-	return a.Advice(a.AdviceLevel.GOOD, "hello world!")
+	source = lib.source(_fileName)
+	usesSpaces = False
+	usesTabs = False
+	for line in source.split("\n"):
+		for char in line:
+			if char == " ":
+				usesSpaces = True
+			elif char == "\t":
+				usesTabs = True
+			else:
+				break
+		if usesSpaces and usesTabs:
+			return a.Advice(a.AdviceLevel.BAD, "use of both tabs and spaces for indentation")
+
+	if usesSpaces:
+		return a.Advice(a.AdviceLevel.GOOD, "only using spaces for indentation")
+	return a.Advice(a.AdviceLevel.GOOD, "only using tabs for indentation")
