9081fd9ea7d81e46c2150114253c16085271516e
Jelleas <jelle.van.assema@gmail.com>
2016-08-28 18:34:22 +0200

    removed commented out code

--- design/advisors/indentationLevel.py
+++ design/advisors/indentationLevel.py
@@ -106,34 +106,3 @@ def getIndentationLevel(line):
 		indentationLevel += 1
 		line = line[len(_indentationStep):]
 	return indentationLevel
-
-
-"""
-def advice():
-	source = lib.removeComments(lib.source(_fileName))
-
-	indentationStep = getIndentationStep(source)
-	indentationLevel = 0
-	mustMatchIndentation = False
-	for line in (line for line in source.split("\n") if len(line.strip()) != 0):
-		if mustMatchIndentation and not line.startswith(indentationStep * indentationLevel):
-			return a.Advice(a.AdviceLevel.BAD, ADVICEMESSAGE, "on line: {}".format(line.strip()))
-		elif not line.startswith(indentationStep * indentationLevel):
-			indentationLevel = 0
-			tempLine = line
-			while tempLine.startswith(indentationStep):
-				indentationLevel += 1
-				tempLine = tempLine[len(indentationStep):]
-		else:
-			tempLine = line[len(indentationStep * indentationLevel):]
-			if tempLine.startswith(" ") or tempLine.startswith("\t"):
-				return a.Advice(a.AdviceLevel.BAD, ADVICEMESSAGE, "on line: {}".format(line.strip()))
-
-		if line.strip().endswith(":"):
-			indentationLevel += 1
-			mustMatchIndentation = True
-		else:
-			mustMatchIndentation = False
-
-	return a.Advice(a.AdviceLevel.GOOD, ADVICEMESSAGE)
-"""
